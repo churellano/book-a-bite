58b6052d8181f68ac3d0c6308c96665e4500ea2f
Jeremiah Chu Arellano <33014733+churellano@users.noreply.github.com>
2022-07-12 14:52:01 -0700

    Add reset table button and more colors

--- src/owner/OwnerRestaurantMap.js
+++ src/owner/OwnerRestaurantMap.js
@@ -1,46 +1,93 @@
-import { Box, Container, Button } from "@mui/material";
+import { Box, Container, Button, ButtonGroup, TextField } from "@mui/material";
 import { useState } from "react";
 
 import Navbar from "../common-components/Navbar"
 import "./OwnerRestaurantMap.css"
 
-// const onClick
+const getCellColour = (cell) => {
+  let color = '#ffffff';
+  if (cell.selected && cell.isPartOfTable) {
+    color = '#0000ff';
+  } else if (cell.selected && !cell.isPartOfTable) {
+    color = '#00ff00';
+  }
+
+  return color;
+}
 
 export default function OwnerRestaurantMap() {
   const test = 10
   const [rows, setRows] = useState(test);
   const [columns, setColumns] = useState(test)
   const [cells, setCells] = useState(Array.from({ length: rows * columns}).map(
-    (e, i) => ({ x: i % columns, y: Math.floor(i / rows), selected: false})
+    (e, i) => ({
+        x: i % columns,
+        y: Math.floor(i / rows),
+        selected: false,
+        isPartOfTable: false
+      })
   ));
   const [tables, setTables] = useState([]);
-  const [currentTable, setCurrentTable] = useState({ cells: [], capacity: null });
+  // const [currentTable, setCurrentTable] = useState({ cells: [], capacity: null }); 
   const [isCreatingTable, setIsCreatingTable] = useState(false);
+  const [guestCapacity, setGuestCapacity] = useState(0);
 
   const gridTemplateStyle = `repeat(${rows}, 1fr) / repeat(${columns}, 1fr)`
 
   const startCreatingTable = () => {
     // TODO: go into creating table mode, allow clicking on grid
+    // setCurrentTable({ cells: [], capacity: null });
     setIsCreatingTable(true);
-
   }
 
   const finishTable = () => {
-    // TODO: find all newly clicked cells and create table object
+    const table = {
+      capacity: guestCapacity,
+      cells: cells.filter(c => c.selected === true)
+    };
+
+    cells.forEach(c => {
+      if (c.selected) {
+        c.isPartOfTable = true;
+      }
+    })
+
+    // Remove new property so that tables created after do not include these cells
+    setCells([...cells])
+
+    // Add new table to array
+    setTables([...tables, table])
+    setGuestCapacity(0);
     setIsCreatingTable(false);
   };
 
+  // Resets selected cells
+  const cancelTable = () => {
+    cells.forEach(c => {
+      if (c.selected && !c.isPartOfTable) {
+        c.selected = false;
+      }
+    });
+
+    setCells([...cells]);
+
+    setIsCreatingTable(false);
+  };
+
+  // TODO: make post request to back end to save
+  const saveRestaurantMap = () => {
+    const restaurantMap = { tables };
+    console.log('Debug: Saving ', restaurantMap);
+  };
+
+  // Mark cells as being part of a table
   const onCellClick = (clickedCell) => (clickObject) => {
     if (isCreatingTable) {
       cells.forEach(c => {
-        console.log(c.x, clickedCell.x)
-        console.log(c.y, clickedCell.y)
-        if (c.x === clickedCell.x && c.y === clickedCell.y) {
-          c.new = true;
+        if (c.x === clickedCell.x && c.y === clickedCell.y && !c.isPartOfTable) {
           c.selected = !c.selected;
         }
       });
-      
       setCells([...cells]);
     }
   }
@@ -49,12 +96,43 @@ export default function OwnerRestaurantMap() {
     <div>
       <Navbar isGuestMode={false} />
       <Container>
-        <Button onClick={startCreatingTable}>l Create Table Mode</Button>
-        <div className="restaurant-map" style={{gridTemplate: gridTemplateStyle}}>
+        <Box sx={{
+          '& > *': {
+            m: 1,
+          },
+        }}>
+          <Button variant="contained" onClick={startCreatingTable} disabled={isCreatingTable}>Start Creating A Table</Button>
+          <ButtonGroup ml={2}>
+            <Button variant="contained" color="success" onClick={finishTable}  disabled={!isCreatingTable || !guestCapacity}>Finish Table</Button>
+            <Button variant="contained" onClick={cancelTable}  disabled={!isCreatingTable}>Cancel Table</Button>
+          </ButtonGroup>
+          <Button variant="outlined" onClick={saveRestaurantMap} disabled={isCreatingTable}>Save Restaurant Map</Button>
+        </Box>
+        <TextField
+          label="Guest capacity for this table"
+          type="number"
+          InputLabelProps={{
+            shrink: true,
+          }}
+          variant="standard"
+          value={guestCapacity}
+          onChange={e => setGuestCapacity(e.target.value)}
+          disabled={!isCreatingTable}
+        />
+        <Box className="restaurant-map" mt={2} style={{gridTemplate: gridTemplateStyle}}>
           {
-            cells.map((cell, i) => <div key={i} className="table-cell" onClick={onCellClick(cell)} style={{ backgroundColor: cell.selected ? '#00ff00' : '#ffffff'}}>{`(${cell.x}, ${cell.y})`}</div>)
+            cells.map((cell, i) => (
+              <div 
+                key={i}
+                className="table-cell"
+                onClick={onCellClick(cell)}
+                style={{ backgroundColor: getCellColour(cell)}}
+              >
+                {`(${cell.x}, ${cell.y})`}
+              </div>
+            ))
           }
-        </div>
+        </Box>
       </Container>
     </div>
   );
