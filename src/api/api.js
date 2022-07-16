e2d621174392fc3266656a60beeba59ada28e928
rshimaro <rshimaro@sfu.ca>
2022-07-15 18:35:43 -0700

    Replace api path by localhost:8080

--- src/api/api.js
+++ src/api/api.js
@@ -1,42 +1,30 @@
 import axios from "axios";
 
 export const addGuest = (data) => {
-  return axios.post(
-    `http://${process.env.REACT_APP_API_BASE_URL}/api/addGuest`,
-    {
-      fname: data.get("firstname"),
-      lname: data.get("lastname"),
-      phone: data.get("phone"),
-      email: data.get("email"),
-    }
-  );
+  return axios.post(`http://localhost:8080/api/addGuest`, {
+    fname: data.get("firstname"),
+    lname: data.get("lastname"),
+    phone: data.get("phone"),
+    email: data.get("email"),
+  });
 };
 
 export const addOwner = (data) => {
-  return axios.post(
-    `http://${process.env.REACT_APP_API_BASE_URL}/api/addOwner`,
-    {
-      fname: data.get("firstname"),
-      lname: data.get("lastname"),
-      email: data.get("email"),
-    }
-  );
+  return axios.post(`http://localhost:8080/api/addOwner`, {
+    fname: data.get("firstname"),
+    lname: data.get("lastname"),
+    email: data.get("email"),
+  });
 };
 
 export const loginGuest = (email) => {
-  return axios.post(
-    `http://${process.env.REACT_APP_API_BASE_URL}/api/guest/login`,
-    {
-      email: email,
-    }
-  );
+  return axios.post(`http://localhost:8080/api/guest/login`, {
+    email: email,
+  });
 };
 
 export const loginOwner = (email) => {
-  return axios.post(
-    `http://${process.env.REACT_APP_API_BASE_URL}/api/owner/login`,
-    {
-      email: email,
-    }
-  );
+  return axios.post(`http://localhost:8080/api/owner/login`, {
+    email: email,
+  });
 };
