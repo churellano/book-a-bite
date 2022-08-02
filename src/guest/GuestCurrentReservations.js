a160e7ca261877b0d84daa076a23b5c545b322cd
Jeremiah Chu Arellano <33014733+churellano@users.noreply.github.com>
2022-08-01 17:20:06 -0700

    Add loaading spinners and placeholder text for empty results

--- src/guest/GuestCurrentReservations.js
+++ src/guest/GuestCurrentReservations.js
@@ -1,17 +1,20 @@
 import { useState, useEffect } from 'react'
-import { Grid, Typography } from '@mui/material'
+import { Box, CircularProgress, Container, Grid, Link, Typography } from '@mui/material'
 
 import Navbar from '../common-components/Navbar'
 import RestaurantListItem from '../common-components/RestaurantListItem'
 import { getCurrentReservationsGuest } from '../api/api'
 
 export default function GuestCurrentReservations(props) {
-    const [restaurant, setRestraurants] = useState([])
+    const [isLoading, setIsLoading] = useState(true);
+
+    const [restaurants, setRestaurants] = useState([])
 
     useEffect(() => {
         getCurrentReservationsGuest(sessionStorage.getItem('userId'))
             .then((res) => {
-                setRestraurants(res.data)
+                setRestaurants(res.data);
+                setIsLoading(false);
             })
             .catch((err) => console.error(err))
     }, [])
@@ -22,21 +25,40 @@ export default function GuestCurrentReservations(props) {
             <Typography variant="h4" component="div" mb={3} textAlign="center">
                 Your Current Reservations
             </Typography>
-            <Grid container spacing={2}>
-                {restaurant.map((rest) => (
-                    <Grid
-                        key={rest.address + rest.reservationid}
-                        item
-                        xs={12}
-                        sm={6}
-                    >
-                        <RestaurantListItem
-                            data={rest}
-                            page="guestCurrentReservations"
-                        />
-                    </Grid>
-                ))}
-            </Grid>
+            <Container>
+                {isLoading ? (
+                    <Box sx={{
+                        display: 'flex',
+                        justifyContent: 'center'
+                    }}>
+                        <CircularProgress />
+                    </Box> 
+                ) : restaurants.length ? (
+                        <Grid container spacing={2}>
+                            {restaurants.map((rest) => (
+                                <Grid
+                                    key={rest.address + rest.reservationid}
+                                    item
+                                    xs={12}
+                                    sm={6}
+                                >
+                                    <RestaurantListItem
+                                        data={rest}
+                                        page="guestCurrentReservations"
+                                    />
+                                </Grid>
+                            ))}
+                        </Grid> 
+                    ) : (
+                        <Box sx={{
+                            textAlign: 'center'
+                        }}>
+                            <Typography>You have no upcoming reservations.</Typography>
+                            <Link href='/guest/main'>Why don't we change that?</Link>
+                        </Box>
+                    )
+                }
+            </Container>
         </div>
     )
 }
